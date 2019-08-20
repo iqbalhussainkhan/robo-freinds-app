ea641f75f57b08da424dfa6d263845e74ab3645c
iqbal khan <iqbalhussainkhan@yahoo.com>
2019-08-20 11:47:38 +0400

    Updates

---
+++ precache-manifest.5d9c97ee9074d25f5fcb3b75a698e041.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7de8e69b5aa530e39b99b6d8ec26629",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d0d43ba101a8bd6b06d",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "e6f99fc8eef4f27eaabc",
+    "url": "/static/css/main.c14e9ae8.chunk.css"
+  },
+  {
+    "revision": "0d0d43ba101a8bd6b06d",
+    "url": "/static/js/2.5c8c54de.chunk.js"
+  },
+  {
+    "revision": "e6f99fc8eef4f27eaabc",
+    "url": "/static/js/main.a12bc647.chunk.js"
+  },
+  {
+    "revision": "5c8c5e8d5ce5b74655cd",
+    "url": "/static/js/runtime~main.66d6610d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
